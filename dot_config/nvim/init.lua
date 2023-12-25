a35c5a043d6dd469017fddb29ded08246979382f
Arman <rostamborne@gmail.com>
2023-12-25 16:57:12 +0330

    Remove .config/nvim/.gitignore Remove .config/nvim/after/plugin/harpoon.lua Remove .config/nvim/after/plugin/lsp.lua Remove .config/nvim/after/plugin/rust-tools.lua Remove .config/nvim/after/plugin/telescope.lua Remove .config/nvim/after/plugin/treesitter.lua Remove .config/nvim/after/plugin/undotree.lua Remove .config/nvim/.git/COMMIT_EDITMSG Remove .config/nvim/.git/FETCH_HEAD Remove .config/nvim/.git/HEAD Remove .config/nvim/.git/ORIG_HEAD Remove .config/nvim/.git/config Remove .config/nvim/.git/description Remove .config/nvim/.git/branches/.keep Remove .config/nvim/.git/hooks/applypatch-msg.sample Remove .config/nvim/.git/hooks/commit-msg.sample Remove .config/nvim/.git/hooks/fsmonitor-watchman.sample Remove .config/nvim/.git/hooks/post-update.sample Remove .config/nvim/.git/hooks/pre-applypatch.sample Remove .config/nvim/.git/hooks/pre-commit.sample Remove .config/nvim/.git/hooks/pre-merge-commit.sample Remove .config/nvim/.git/hooks/pre-push.sample Remove .config/nvim/.git/hooks/pre-rebase.sample Remove .config/nvim/.git/hooks/pre-receive.sample Remove .config/nvim/.git/hooks/prepare-commit-msg.sample Remove .config/nvim/.git/hooks/push-to-checkout.sample Remove .config/nvim/.git/hooks/update.sample Remove .config/nvim/.git/info/exclude Remove .config/nvim/.git/logs/HEAD Remove .config/nvim/.git/logs/refs/heads/master Remove .config/nvim/.git/logs/refs/remotes/origin/HEAD Remove .config/nvim/.git/logs/refs/remotes/origin/master Remove .config/nvim/.git/objects/0f/34e9e39ec77cb3cede17e7a5b4c0078bb061c2 Remove .config/nvim/.git/objects/21/f81b94e38d28034c2ff5fd7341914a0d04098c Remove .config/nvim/.git/objects/29/d96c45c7f24bd56445aa476622923d16835c59 Remove .config/nvim/.git/objects/4e/1a167afa3df2bfb896a23dd34084b2b5c8201d Remove .config/nvim/.git/objects/53/26aecb87e49619694682642059438cb9065eab Remove .config/nvim/.git/objects/75/d6710a3951c12ec8ba7c1ae07d28169814c2a6 Remove .config/nvim/.git/objects/8c/b205e9b3de5355dfc99714f03eb966ed69b9da Remove .config/nvim/.git/objects/eb/51ede6a69d970ff950c256f121e3d2626f2375 Remove .config/nvim/.git/objects/info/.keep Remove .config/nvim/.git/objects/pack/pack-fa41aadeccbb26dcf1f8c1f9cdd908d175b4d897.idx Remove .config/nvim/.git/objects/pack/pack-fa41aadeccbb26dcf1f8c1f9cdd908d175b4d897.pack Remove .config/nvim/.git/refs/heads/master Remove .config/nvim/.git/refs/remotes/origin/HEAD Remove .config/nvim/.git/refs/remotes/origin/master Remove .config/nvim/.git/refs/tags/.keep Remove .config/nvim/.git/index Remove .config/nvim/.git/packed-refs Remove .config/nvim/plugin/packer_compiled.lua Remove .config/nvim/init.lua Remove .config/nvim/lazy-lock.json Remove .config/nvim/lua/arman/config/keymap.lua Remove .config/nvim/lua/arman/config/sets.lua Remove .config/nvim/lua/arman/init.lua Remove .config/nvim/lua/arman/packer.lua Remove .config/nvim/lua/arman/plugins/colorscheme.lua Remove .config/nvim/lua/arman/plugins/harpoon.lua Remove .config/nvim/lua/arman/plugins/lazy.lua Remove .config/nvim/lua/arman/plugins/lsp-config.lua Remove .config/nvim/lua/arman/plugins/telescope.lua Remove .config/nvim/lua/arman/plugins/treesitter.lua Remove .config/nvim/lua/arman/remap.lua Remove .config/nvim/lua/arman/set.lua Remove .config/nvim/lua/.luarc.json

--- dot_config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
-require("arman.plugins.lazy")
-require("arman.plugins.colorscheme")
-require("arman.config.sets")
-require("arman.config.keymap")
-require("arman.plugins.treesitter")
-require("arman.plugins.telescope")
-require("arman.plugins.harpoon")
-require("arman.plugins.lsp-config")
